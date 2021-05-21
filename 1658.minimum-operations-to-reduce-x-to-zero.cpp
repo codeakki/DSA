d37c5666f47f24baef2906e615ca51b9308ffee5
Akshay Sharama <76440986+codeakki@users.noreply.github.com>
2021-05-21 21:32:18 +0530

    Add files via upload

---
+++ 1658.minimum-operations-to-reduce-x-to-zero.cpp
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=1658 lang=cpp
+ *
+ * [1658] Minimum Operations to Reduce X to Zero
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) {
+
+
+        for( auto a:nums){
+            
+        }
+        
+    }
+};
+// @lc code=end
+
