b008bef4971e01e5f7e4064d3492e71ac208710b
Akshay Sharama <76440986+codeakki@users.noreply.github.com>
2021-05-26 23:07:56 +0530

    Add files via upload

---
+++ 53.maximum-subarray.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=53 lang=cpp
+ *
+ * [53] Maximum Subarray
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+          int sum = 0, smax = INT_MIN;
+        for (int num : nums) {
+            sum += num;
+            smax = max(smax, sum);
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return smax;
+        
+    }
+};
+// @lc code=end
+
