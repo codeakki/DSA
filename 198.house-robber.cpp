dbe52537bbc3ab6dbf2346cd258035f9f8d73d7f
Akshay Sharama <76440986+codeakki@users.noreply.github.com>
2021-05-27 21:29:00 +0530

    Add files via upload

---
+++ 198.house-robber.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=198 lang=cpp
+ *
+ * [198] House Robber
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        const int n = nums.size();
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
+        if (n == 2) return max(nums[0], nums[1]);
+        vector<int> f(n, 0);
+        f[0] = nums[0];
+        f[1] = max(nums[0], nums[1]);
+        for (int i = 2; i < n; ++i)
+            f[i] = max(f[i-2] + nums[i], f[i-1]);
+        return f[n-1];
+        
+    }
+};
+// @lc code=end
+
