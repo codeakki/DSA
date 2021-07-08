ae5b56bd321ee0c56fa4b0e7ad39fdbe4341c034
Akshay Sharama <76440986+codeakki@users.noreply.github.com>
2021-07-08 21:52:34 +0530

    Add files via upload

---
+++ 31.next-permutation.cpp
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode id=31 lang=cpp
+ *
+ * [31] Next Permutation
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    void nextPermutation(vector<int> &nums)
+    {
+        int n = nums.size(), k, l;
+        for (k = n - 2; k >= 0; k--)
+        {
+            if (nums[k] < nums[k + 1])
+            {
+                break;
+            }
+        }
+        if (k < 0)
+        {
+            reverse(nums.begin(), nums.end());
+        }
+        else
+        {
+            for (l = n - 1; l > k; l--)
+            {
+                if (nums[l] > nums[k])
+                {
+                    break;
+                }
+            }
+            swap(nums[k], nums[l]);
+            reverse(nums.begin() + k + 1, nums.end());
+        }
+    }
+};
+// @lc code=end
